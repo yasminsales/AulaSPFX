15e2245e74372833b9a087ad709806732637f926
Yasmin Sales <teacheryasminsales@gmail.com>
2021-10-18 16:16:26 -0300

    Gravando form

--- Webpart/src/webparts/helloWorld2/components/form/index.tsx
+++ Webpart/src/webparts/helloWorld2/components/form/index.tsx
@@ -8,10 +8,9 @@ import "@pnp/sp/items";
 import { setVirtualParent } from '@fluentui/dom-utilities';
 
 type Inputs = {
-    FirstName: string,
-    LastName: string,
     Title: string,
-    Description: string,
+    Lastname: string,
+    EmailAddress: string,
 };
 
 export default function Form(props) {
@@ -35,26 +34,21 @@ export default function Form(props) {
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                 <label htmlFor="exampleInputFirstName1" className="form-label">First name</label>
-                <input ref={register({ required: true, maxLength: 20 })} name="First name" type="name" className="form-control" id="exampleInputFirstName1"></input>
+                <input ref={register({ required: true, maxLength: 20 })} name="Title" type="name" className="form-control" id="exampleInputFirstName1"></input>
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleInputLastName1" className="form-label">Last name</label>
-                <input ref={register({ required: true, maxLength: 50 })} name="First name" type="name" className="form-control" id="exampleInputLastName1"></input>
+                <input ref={register({ required: true, maxLength: 50 })} name="Lastname" type="name" className="form-control" id="exampleInputLastName1"></input>
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input ref={register} name="Title" type="text" className="form-control" id="exampleInputEmail1" />
+                <input ref={register} name="EmailAddress" type="text" className="form-control" id="exampleInputEmail1" />
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input ref={register({ required: true })} name="Description" type="password" className="form-control" id="exampleInputPassword1"></input>
             </div>
             <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
                 <label className="form-check-label">Check me out</label>
             </div>
-            {errors.Description && <span>This field is required</span>}
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
